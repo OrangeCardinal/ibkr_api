10807fddeaa31905bbacc4c0d613662bf4d4c949
OrangeCardinal <david.scott.orkin@gmail.com>
2018-11-27 07:03:13 -0700

    Start of 'helper' classes. Dow30 is a list of Dow 30 Stock objects. Nasdaq, etc can and should be added, but more refactoring is first needed 1. To auto generate the classes and methods themselves. 2. Add logic to select dow 30 members over time.
    After those two tasks are done, I can go forward with the SP, Nasdaq, Russel, etc

---
+++ classes/stocks/dow30.py
@@ -0,0 +1,431 @@
+from classes.contracts.contract import Contract
+
+
+class Dow30():
+    def AAPL(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "AAPL"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def AXP(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "AXP"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def BA(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "BA"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def CAT(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "CAT"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def CSCO(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "CSCO"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def CVX(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "CVX"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def DIS(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "DIS"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def DWDP(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "DWDP"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def GS(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "GS"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def HD(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "HD"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def IBM(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "IBM"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def INTC(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "INTC"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def JNJ(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "JNJ"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def JPM(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "JPM"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def KO(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "KO"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def MCD(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "MCD"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def MMM(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "MMM"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def MRK(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "MRK"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def MSFT(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "MSFT"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def NKE(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "NKE"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def PFE(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "PFE"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def PG(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "PG"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def TRV(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "TRV"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def UNH(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "UNH"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def UTX(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "UTX"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def V(self):
+        """
+        V Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "V"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def VZ(self):
+        """
+        VZ Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "VZ"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def WBA(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "WBA"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def WMT(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "WMT"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+    def XOM(self):
+        """
+        AAPL Stock (Contract)
+        :return:
+        """
+        contract = Contract()
+        contract.symbol = "XOM"
+        contract.security_type = "STK"
+        contract.currency = "USD"
+        #In the API side, NASDAQ is always defined as ISLAND in the exchange field
+        contract.exchange = "ISLAND"
+        #! [stkcontract]
+        return contract
+
+
+    def stocks(self):
+        stocks = [self.AAPL(), self.AXP(),self.BA(), self.CAT(), self.CSCO(), self.CVX(), self.DIS(), self.DWDP(), self.GS(),
+                  self.HD(), self.IBM(), self.INTC(), self.JNJ(), self.JPM(), self.KO(), self.MCD(), self.MMM(), self.MRK(),
+                  self.MSFT(), self.NKE(), self.PFE(), self.PG(), self.TRV(), self.UNH(), self.UTX(), self.V(), self.VZ(),
+                  self.WBA(), self.WMT(), self.XOM()]
+        yield from stocks
