b80f26f14d02e43f61889aa7486785576ef70748
OrangeCardinal <david.scott.orkin@gmail.com>
2018-12-02 11:32:46 -0700

    Histogram bar object.

---
+++ classes/bar.py
@@ -0,0 +1,10 @@
+class Bar(object):
+    def __init__(self):
+        self.date = ""
+        self.open = 0.
+        self.high = 0.
+        self.low = 0.
+        self.close = 0.
+        self.volume = 0
+        self.bar_count = 0
+        self.average = 0.
