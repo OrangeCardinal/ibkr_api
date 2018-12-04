0e915515ffba6ceed15c5ef12b9525065b76b30e
OrangeCardinal <david.scott.orkin@gmail.com>
2018-12-04 16:17:42 -0700

    Enum of the various accepted values for the bar_size parameters

---
+++ classes/bar_size.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class BarSize(Enum):
+    SECONDS_1 = "1 sec"
+    SECONDS_5 = "5 secs"
+    SECONDS_15 = "15 secs"
+    SECONDS_30 = "30 secs"
+    MINUTES_1 = "1 min"
+    MINUTES_2 = "2 mins"
+    MINUTES_3 = "3 mins"
+    MINUTES_5 = "5 mins"
+    MINUTES_15 = "15 mins"
+    MINUTES_30 = "30 mins"
+    HOUR_1  = "1 hour"
+    DAY_1   = "1 day"
