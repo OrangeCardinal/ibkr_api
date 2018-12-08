f2f606346c5dec3d00a97673edfbbab1f30aa93f
OrangeCardinal <david.scott.orkin@gmail.com>
2018-12-08 11:12:22 -0700

    Legal Rule 80 A values.

---
+++ classes/enum/rule_80a.py
@@ -0,0 +1,11 @@
+from enum import Enum
+class Rule80A(Enum):
+    INDIVIDUAL              = 'I',
+    AGENCY                  = 'A',
+    AGENT_OTHER_MEMBER      = 'W',
+    INDIVIDUAL_PTIA         = 'J',
+    AGENCY_PTIA             = 'U',
+    AGENT_OTHER_MEMBER_PTIA = 'M',
+    INDIVIDUAL_PT           = 'K',
+    AGENCY_PT               = 'Y',
+    AGENT_OTHER_MEMBER_PT   = 'N'
