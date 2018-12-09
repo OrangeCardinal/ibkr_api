aaa910ebb185a501e4b1bca99a3ea05a9c822593
OrangeCardinal <david.scott.orkin@gmail.com>
2018-12-09 10:01:59 -0700

    Removed old classes (Logically they always should have been part of contract). Added formatting in the __str__ function for Regular Trading Hours.

--- classes/contracts/contract.py
+++ classes/contracts/contract.py
@@ -21,16 +21,19 @@ class Contract(object):
         self.security_type = security_type
         self.symbol = symbol
         self.id = contract_id
-        self.last_trade_date_or_contract_month = last_trade_date_or_contract_month
-        self.strike = strike # A float is expected
-        self.right = right
-        self.multiplier = multiplier
-        self.primary_exchange = primary_exchange # pick an actual (ie non-aggregate) exchange that the contract trades on.  DO NOT SET TO SMART.
-        self.local_symbol = local_symbol
-        self.trading_class = trading_class
-        self.include_expired = include_expired
-        self.security_id_type = security_id_type	  # CUSIP;SEDOL;ISIN;RIC
-        self.security_id = security_id
+        self.last_trade_date_or_contract_month  = last_trade_date_or_contract_month
+        self.strike                             = strike # A float is expected
+        self.right                              = right
+        self.multiplier                         = multiplier
+        # Primary exchange should be an actual (ie non-aggregate) exchange that the contract trades on.
+        # DO NOT SET TO SMART.
+        self.primary_exchange                   = primary_exchange
+        self.local_symbol                       = local_symbol
+        self.trading_class                      = trading_class
+        self.include_expired                    = include_expired
+        self.regular_trading_hours              = {}
+        self.security_id_type                   = security_id_type	  # CUSIP;SEDOL;ISIN;RIC
+        self.security_id                        = security_id
 
         self.derivative_security_types = []
         #combos
@@ -51,8 +54,20 @@ class Contract(object):
         """
         desc =  "\nContract\n"
         desc +=  "--------\n"
-        desc += "ID: {0}\nSymbol: {1}\nSecurity Type: {2}".format(self.id, self.symbol, self.security_type)
+        desc += "ID: {0}\nSymbol: {1}\nSecurity Type: {2}\n".format(self.id, self.symbol, self.security_type)
         desc += "Exchange: {0}\n".format(self.exchange)
+
+
+        if len(self.regular_trading_hours) > 0:
+            desc += "\nRegular Trading Hours\n"
+            desc += "---------------------\n"
+            for day in self.regular_trading_hours.keys():
+                hours = self.regular_trading_hours[day]
+                if len(hours) == 2:
+                    hours = "{0} - {1}".format(hours[0], hours[1])
+                else:
+                    hours = hours[0]
+                desc += "{0}: {1}\n".format(day, hours)
         """
         if self.comboLegs:
             for leg in self.comboLegs:
@@ -62,99 +77,3 @@ class Contract(object):
             s += ";" + str(self.deltaNeutralContract)
         """
         return desc
-
-
-
-class ContractDetails(object):
-    def __init__(self):
-        self.contract = Contract()
-        self.marketName = ""
-        self.minTick = 0.
-        self.orderTypes = ""
-        self.valid_exchanges = ""
-        self.price_magnifier = 0
-        self.underConId = 0
-        self.longName = ""
-        self.contractMonth = ""
-        self.industry = ""
-        self.category = ""
-        self.subcategory = ""
-        self.timeZoneId = ""
-        self.tradingHours = ""
-        self.liquidHours = ""
-        self.evRule = ""
-        self.evMultiplier = 0
-        self.mdSizeMultiplier = 0
-        self.aggGroup = 0
-        self.underSymbol = ""
-        self.underSecType = ""
-        self.marketRuleIds = ""
-        self.security_id_list = None
-        self.realExpirationDate = ""
-        self.lastTradeTime = ""
-        # BOND values
-        self.cusip = ""
-        self.ratings = ""
-        self.descAppend = ""
-        self.bondType = ""
-        self.couponType = ""
-        self.callable = False
-        self.putable = False
-        self.coupon = 0
-        self.convertible = False
-        self.maturity = ""
-        self.issueDate = ""
-        self.next_option_date = ""
-        self.next_option_type = ""
-        self.next_option_partial = False
-        self.notes = ""
-
-    def __str__(self):
-        s = ",".join((
-            str(self.contract),
-            str(self.marketName),
-            str(self.minTick),
-            str(self.orderTypes),
-            str(self.valid_exchanges),
-            str(self.price_magnifier),
-            str(self.underConId),
-            str(self.longName),
-            str(self.contractMonth),
-            str(self.industry),
-            str(self.category),
-            str(self.subcategory),
-            str(self.timeZoneId),
-            str(self.tradingHours),
-            str(self.liquidHours),
-            str(self.evRule),
-            str(self.evMultiplier),
-            str(self.mdSizeMultiplier),
-            str(self.underSymbol),
-            str(self.underSecType),
-            str(self.marketRuleIds),
-            str(self.aggGroup),
-            str(self.security_id_list),
-            str(self.realExpirationDate),
-            str(self.cusip),
-            str(self.ratings),
-            str(self.descAppend),
-            str(self.bondType),
-            str(self.couponType),
-            str(self.callable),
-            str(self.putable),
-            str(self.coupon),
-            str(self.convertible),
-            str(self.maturity),
-            str(self.issueDate),
-            str(self.next_option_date),
-            str(self.next_option_type),
-            str(self.next_option_partial),
-            str(self.notes)))
-        return s
-
-
-
-class ContractDescription(object):
-    def __init__(self):
-        self.contract = Contract()
-        self.derivativeSecTypes = None   # type: list of strings
