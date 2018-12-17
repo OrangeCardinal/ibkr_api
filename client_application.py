03bd41abad3af9aa878e090fec452062725e78ae
OrangeCardinal <david.scott.orkin@gmail.com>
2018-12-16 21:41:51 -0700

    Primary event loop is working as designed. Switched to unpacking argument lists to allow the post response functions to be more readable. (vs having a parameter like "data")

--- client_application.py
+++ client_application.py
@@ -14,10 +14,15 @@ class ClientApplication(ApiCalls):
         :param response_handler: User Supplied Response Handler
         :param request_handler:
         """
-        self.still_running     = False # Controls when the event loop
-        self.messages_received = []    # List of all messages received (not sure if needed)
-        self.keyboard_input    = []    # Keyboard Input
-        self.message_parser    = MessageParser()
+        self.still_running      = True  # Controls when the event loop
+        self.messages_received  = []    # List of all messages received (not sure if needed)
+        self.keyboard_input     = []    # Keyboard Input
+        self.message_parser     = MessageParser()
+        self.client_id          = 0
+        self.debug_mode         = True
+
+        super().__init__()
+        super().connect(host, port, self.client_id)
 
     def act(self):
         """
@@ -26,14 +31,20 @@ class ClientApplication(ApiCalls):
         """
         raise NotImplementedError
 
-    def on_start(self):
+
+    def info_message(self, message_id, request_id, info):
+        print(info)
+
+    def initialize(self):
         """
-        Initial actions that your application will do on startup.
+        Called one time before the main event processing loop is entered.
         :return:
         """
         raise NotImplementedError
 
-
+    def managed_accounts(self,message_id, request_id, account):
+        print(request_id)
+        print(account)
 
     # TODO: Handle keyboard input in a non blocking manner
     def run(self):
@@ -47,32 +58,35 @@ class ClientApplication(ApiCalls):
         # keyboard_reader = KeyboardReader()
         # keyboard_reader.start()
 
-        self.on_start()
+        self.initialize()
 
         while self.still_running:
             messages = self.conn.receive_messages()
             for message in messages:
                     # Call the correct function in the message parser to parse and return the data
+                    logging.debug("{0} message received".format(message['action']))
                     func = getattr(self.message_parser, message['action'])
                     data = func(message['fields'])
 
                     # Call the response handler as needed
-                    #TODO: Figure out if we care about the return value here
-                    func = getattr(self.response_handler, message['action'])
-                    func(data)
-
+                    # If we are in development also send a warning that a handler doesnt exist
+                    if hasattr(self, message['action']):
+                        func = getattr(self, message['action'])
+                        func(*data)
+                    elif self.debug_mode:
+                        logger.warning("The function '{0}' does not exist.".format(message['action']))
 
             # Invoke any user defined behaviour at this point.
             self.act()
-            time.sleep(2)
+            time.sleep(1)
 
-        logger.info("Application is now shut down.")
+        logger.info("Application has been shut down.")
         return 0
 
     def stop(self):
         """
-        Stops the primary event loop
+        Stops the primary event loop, allowing the application to
         :return:
         """
-        logger.info("Application is now shutting down.")
+        logger.info("Shutdown requested")
         self.still_running = False
