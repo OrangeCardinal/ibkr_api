483bc8bece3f311337f213986e88693fe8ee3761
OrangeCardinal <david.scott.orkin@gmail.com>
2018-12-29 23:39:40 -0700

    Improved output in the __str__ function. Started a to_csv function.

--- ibkr_api/classes/execution.py
+++ ibkr_api/classes/execution.py
@@ -4,28 +4,40 @@ class Execution(object):
     """
 
     def __init__(self):
-        self.id = ""
-        self.time =  ""
-        self.account_number = ""
-        self.exchange =  ""
-        self.side = ""
-        self.shares = 0.
-        self.price = 0.
-        self.perm_id = 0
-        self.clientId = 0
-        self.orderId = 0
-        self.liquidation = 0
-        self.cumQty = 0.
-        self.avgPrice = 0.
-        self.orderRef =  ""
-        self.evRule =  ""
-        self.evMultiplier = 0.
-        self.modelCode =  ""
-        self.lastLiquidity = 0
+        self.id             = ""
 
+        self.account_number = ""
+        self.datetime       = ""
+        self.exchange       =  ""
+        self.side           = ""
+        self.shares         = 0.
+        self.price          = 0.
+        self.perm_id        = 0
+        self.client_id      = 0
+        self.order_id       = 0
+        self.liquidation    = 0
+        self.quantity       = 0.
+        self.average_price  = 0.
+        self.order_reference    =  ""
+        self.evRule             =  ""
+        self.evMultiplier   = 0.
+        self.modelCode      =  ""
+        self.lastLiquidity  = 0
+        
+        self.contract       = None
+
+
+    def to_csv(self):
+        fields = [self.id, self.contract.id]
+        return ",".join(fields)
+        
     def __str__(self):
-        return "ID: %s, Time: %s, Account: %s, Exchange: %s, Side: %s, Shares: %f, Price: %f, PermId: %d, " \
-                "ClientId: %d, OrderId: %d, Liquidation: %d, CumQty: %f, AvgPrice: %f, OrderRef: %s, EvRule: %s, " \
-                "EvMultiplier: %f, ModelCode: %s, LastLiquidity: %d" % (self.id, self.time, self.account_number,
-                                                                        self.exchange, self.side, self.shares, self.price, self.perm_id, self.clientId, self.orderId, self.liquidation,
-                                                                        self.cumQty, self.avgPrice, self.orderRef, self.evRule, self.evMultiplier, self.modelCode, self.lastLiquidity)
+        desc  = "Execution Information\n"
+        desc += "---------------------\n"
+        desc += "ID: {0}\n".format(self.datetime)
+        desc += "Order ID: {0}\n".format(self.order_id)
+        desc += "Ordder Ref: {0}\n".format(self.order_reference)
+        desc += "Price: {0}\n".format(self.price)
+        desc += "Average Price: {0}\n".format(self.average_price)
+        desc += "Side: {0}\n".format(self.side)
+        return desc
