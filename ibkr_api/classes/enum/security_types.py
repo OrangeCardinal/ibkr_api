6f6d6931b79c6436543cd57cfc04a8892fdc3fbe
OrangeCardinal <david.scott.orkin@gmail.com>
2019-01-13 08:55:58 -0700

    Removed the commas that should of never been there in the first place. (Caused value to return tuple)

--- ibkr_api/classes/enum/security_types.py
+++ ibkr_api/classes/enum/security_types.py
@@ -1,11 +1,11 @@
 from enum import Enum
 class SecurityTypes(Enum):
-    CASH                        = 'CASH'    ,
-    STOCK                       = 'STK'     ,
-    FUTURES                     = 'FUT'     ,
-    OPTIONS                     = 'OPT'     ,
-    FUTURES_OPTIONS             = 'FOP'     ,
-    WARRANTS                    = 'WAR'     ,
-    BONDS                       = 'BONDS'   ,
-    CONTRACTS_FOR_DIFFERENCES   = 'CFD'     ,
+    CASH                        = 'CASH'
+    STOCK                       = 'STK'
+    FUTURES                     = 'FUT'
+    OPTIONS                     = 'OPT'
+    FUTURES_OPTIONS             = 'FOP'
+    WARRANTS                    = 'WAR'
+    BONDS                       = 'BONDS'
+    CONTRACTS_FOR_DIFFERENCES   = 'CFD'
     DUTCH_WARRANTS              = 'IOPT'
