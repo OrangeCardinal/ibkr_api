a0f834628add0880630f29ffd2a39c97872cba4b
OrangeCardinal <david.scott.orkin@gmail.com>
2019-01-23 10:38:28 -0700

    Changed init to take keyword arguments. Minor formatting / pep8.

--- ibkr_api/classes/orders/order.py
+++ ibkr_api/classes/orders/order.py
@@ -18,7 +18,7 @@ class OrderComboLeg(object):
 
 
 class Order(object):
-    def __init__(self):
+    def __init__(self,**kwargs):
         self.softDollarTier = SoftDollarTier("", "", "")
         # order identifier
         self.order_id  = 0
@@ -26,11 +26,11 @@ class Order(object):
         self.perm_id   = 0
 
         # main order fields
-        self.action             = ""
-        self.total_quantity     = 0
-        self.order_type = ""
-        self.limit_price      = UNSET_DOUBLE
-        self.aux_price      = UNSET_DOUBLE
+        self.action                 = ""
+        self.total_quantity         = 0
+        self.order_type             = ""
+        self.limit_price            = UNSET_DOUBLE
+        self.aux_price              = UNSET_DOUBLE
 
         # extended order fields
         self.contract               = None # Contract object (if any) associated with the order (
@@ -46,17 +46,17 @@ class Order(object):
         self.sweep_to_fill          = False
         self.display_size           = 0
         self.trigger_method         = 0     # 0=Default, 1=Double_Bid_Ask, 2=Last, 3=Double_Last, 4=Bid_Ask, 7=Last_or_Bid_Ask, 8=Mid-point
-        self.outside_rth     = False
-        self.hidden            = False
-        self.good_after_time   = ""   # Format: 20060505 08:00:00 {time zone}
-        self.good_till_date    = ""   # Format: 20060505 08:00:00 {time zone}
-        self.rule80A           = ""   # Individual = 'I', Agency = 'A', AgentOtherMember = 'W', IndividualPTIA = 'J', AgencyPTIA = 'U', AgentOtherMemberPTIA = 'M', IndividualPT = 'K', AgencyPT = 'Y', AgentOtherMemberPT = 'N'
-        self.all_or_none         = False
-        self.min_qty         = UNSET_INTEGER  #type: int
-        self.percent_offset  = UNSET_DOUBLE  # type: float; REL orders only
-        self.override_percentage_constraints = False
-        self.trail_stop_price = UNSET_DOUBLE  # type: float
-        self.trailing_percent = UNSET_DOUBLE # type: float; TRAILLIMIT orders only
+        self.outside_rth            = False
+        self.hidden                 = False
+        self.good_after_time        = ""   # Format: 20060505 08:00:00 {time zone}
+        self.good_till_date         = ""   # Format: 20060505 08:00:00 {time zone}
+        self.rule80A                = ""   # Individual = 'I', Agency = 'A', AgentOtherMember = 'W', IndividualPTIA = 'J', AgencyPTIA = 'U', AgentOtherMemberPTIA = 'M', IndividualPT = 'K', AgencyPT = 'Y', AgentOtherMemberPT = 'N'
+        self.all_or_none            = False
+        self.min_qty                = UNSET_INTEGER  #type: int
+        self.percent_offset                     = UNSET_DOUBLE  # type: float; REL orders only
+        self.override_percentage_constraints    = False
+        self.trail_stop_price                   = UNSET_DOUBLE  # type: float
+        self.trailing_percent                   = UNSET_DOUBLE # type: float; TRAILLIMIT orders only
 
         # Only used by financial advisers
         self.financial_advisers_group              = ""
@@ -65,64 +65,64 @@ class Order(object):
         self.financial_advisers_percentage         = ""
 
         # institutional (ie non-cleared) only
-        self.designatedLocation    = "" #used only when short_sale_slot=2
-        self.open_close             = "O"    # O=Open, C=Close
-        self.origin                = CUSTOMER  # 0=Customer, 1=Firm
-        self.short_sale_slot         = 0    # type: int; 1 if you hold the shares, 2 if they will be delivered from elsewhere.  Only for Action=SSHORT
-        self.exempt_code            = -1
+        self.designated_location            = "" #used only when short_sale_slot=2
+        self.open_close                     = "O"    # O=Open, C=Close
+        self.origin                         = CUSTOMER  # 0=Customer, 1=Firm
+        self.short_sale_slot                = 0    # type: int; 1 if you hold the shares, 2 if they will be delivered from elsewhere.  Only for Action=SSHORT
+        self.exempt_code                    = -1
 
         # SMART routing only
-        self.discretionary_amt = 0
-        self.e_trade_only       = True
-        self.firm_quote_only    = True
-        self.nbbo_price_cap     = UNSET_DOUBLE  # type: float
-        self.opt_out_smart_routing = False
+        self.discretionary_amt              = 0
+        self.e_trade_only                   = True
+        self.firm_quote_only                = True
+        self.nbbo_price_cap                 = UNSET_DOUBLE  # type: float
+        self.opt_out_smart_routing          = False
 
         # BOX exchange orders only
-        self.auctionStrategy = AUCTION_UNSET # type: int; AUCTION_MATCH, AUCTION_IMPROVEMENT, AUCTION_TRANSPARENT
-        self.starting_price   = UNSET_DOUBLE   # type: float
-        self.stock_ref_price   = UNSET_DOUBLE   # type: float
-        self.delta           = UNSET_DOUBLE   # type: float
+        self.auctionStrategy                = AUCTION_UNSET # type: int; AUCTION_MATCH, AUCTION_IMPROVEMENT, AUCTION_TRANSPARENT
+        self.starting_price                 = UNSET_DOUBLE   # type: float
+        self.stock_ref_price                = UNSET_DOUBLE   # type: float
+        self.delta                          = UNSET_DOUBLE   # type: float
 
         # pegged to stock and VOL orders only
-        self.stock_range_lower = UNSET_DOUBLE   # type: float
-        self.stock_range_upper = UNSET_DOUBLE   # type: float
+        self.stock_range_lower              = UNSET_DOUBLE   # type: float
+        self.stock_range_upper              = UNSET_DOUBLE   # type: float
 
-        self.randomize_price = False
-        self.randomize_size = False
+        self.randomize_price                = False
+        self.randomize_size                 = False
 
         # COMBO ORDERS ONLY
-        self.basis_points     = UNSET_DOUBLE  # type: float; EFP orders only
-        self.basis_points_type = UNSET_INTEGER  # type: int;  EFP orders only
+        self.basis_points                   = UNSET_DOUBLE  # type: float; EFP orders only
+        self.basis_points_type              = UNSET_INTEGER  # type: int;  EFP orders only
 
         # SCALE ORDERS ONLY
-        self.scale_init_level_size       = UNSET_INTEGER  # type: int
-        self.scale_subs_level_size       = UNSET_INTEGER  # type: int
-        self.scale_price_increment      = UNSET_DOUBLE  # type: float
-        self.scale_price_adjust_value    = UNSET_DOUBLE  # type: float
-        self.scale_price_adjust_interval = UNSET_INTEGER  # type: int
-        self.scale_profit_offset        = UNSET_DOUBLE  # type: float
-        self.scale_auto_reset           = False
-        self.scale_init_position        = UNSET_INTEGER   # type: int
-        self.scale_init_fill_qty         = UNSET_INTEGER    # type: int
-        self.scale_random_percent       = False
+        self.scale_init_level_size          = UNSET_INTEGER
+        self.scale_subs_level_size          = UNSET_INTEGER
+        self.scale_price_increment          = UNSET_DOUBLE
+        self.scale_price_adjust_value       = UNSET_DOUBLE
+        self.scale_price_adjust_interval    = UNSET_INTEGER
+        self.scale_profit_offset            = UNSET_DOUBLE
+        self.scale_auto_reset               = False
+        self.scale_init_position            = UNSET_INTEGER
+        self.scale_init_fill_qty            = UNSET_INTEGER
+        self.scale_random_percent           = False
         self.scale_table = ""
 
         # HEDGE ORDERS
-        self.hedge_type             = "" # 'D' - delta, 'B' - beta, 'F' - FX, 'P' - pair
-        self.hedge_param            = "" # 'beta=X' value for beta hedge, 'ratio=Y' for pair hedge
+        self.hedge_type                     = "" # 'D' - delta, 'B' - beta, 'F' - FX, 'P' - pair
+        self.hedge_param                    = "" # 'beta=X' value for beta hedge, 'ratio=Y' for pair hedge
 
         # Clearing info
-        self.account               = "" # IB account
-        self.settlingFirm          = ""
-        self.clearing_account       = ""   #True beneficiary of the order
-        self.clearing_intent        = "" # "" (Default), "IB", "Away", "PTA" (PostTrade)
+        self.account                        = "" # IB account
+        self.settling_firm                   = ""
+        self.clearing_account               = ""   #True beneficiary of the order
+        self.clearing_intent                = "" # "" (Default), "IB", "Away", "PTA" (PostTrade)
 
         # ALGO ORDERS ONLY
-        self.algorithmic_strategy          = ""
+        self.algorithmic_strategy           = ""
 
-        self.algorithm_parameters            = None    #TagValueList
-        self.smart_combo_routing_params = None  #TagValueList
+        self.algorithm_parameters           = None    #TagValueList
+        self.smart_combo_routing_params     = None  #TagValueList
 
         self.algoId = ""
 
@@ -140,7 +140,7 @@ class Order(object):
 
         self.order_combo_legs = None  # OrderComboLegListSPtr
 
-        self.orderMiscOptions = None  # TagValueList
+        self.order_misc_options = None  # TagValueList
 
         # VER PEG2BENCH fields:
         self.reference_contract_id            = 0
@@ -175,6 +175,12 @@ class Order(object):
         self.dont_use_auto_price_for_hedge = False
 
         self.is_oms_container = False
+
+
+        # Set any attributes that were supplied via keyword arguments
+        for key, val in kwargs.items():
+            if hasattr(self,key):
+                setattr(self,key,val)
 
     def __str__(self, display_contract=True):
         """
