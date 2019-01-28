e547364359eeb48a76a1d56b36ab71e6d031c604
OrangeCardinal <david.scott.orkin@gmail.com>
2019-01-28 14:50:10 -0700

    Fixed issues with the init function.

--- ibkr_api/classes/orders/limit_order.py
+++ ibkr_api/classes/orders/limit_order.py
@@ -2,6 +2,10 @@ from ibkr_api.classes.orders.order import Order
 
 class LimitOrder(Order):
     def __init__(self, contract, total_quantity, limit_price, **kwargs):
-        super().__init__(kwargs)
-        self.order_type = "LMT"
+        super().__init__(**kwargs)
+        self.contract       =   contract
+        self.order_type     =   "LMT"
+        self.total_quantity =   total_quantity
+        self.limit_price    =   limit_price
+
 
