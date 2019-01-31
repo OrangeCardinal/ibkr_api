e9fa05937d531df2eca65978eb5caf635b15e735
OrangeCardinal <david.scott.orkin@gmail.com>
2019-01-31 12:07:53 -0700

    Added action as one of the parameters for instantiation.

--- ibkr_api/classes/orders/limit_order.py
+++ ibkr_api/classes/orders/limit_order.py
@@ -1,10 +1,11 @@
 from ibkr_api.classes.orders.order import Order
 
 class LimitOrder(Order):
-    def __init__(self, contract, total_quantity, limit_price, **kwargs):
+    def __init__(self, action, contract, total_quantity, limit_price, **kwargs):
         super().__init__(**kwargs)
+        self.action         =   action
         self.contract       =   contract
-        self.order_type     =   "LMT"
+        self.order_type     =   'LMT'
         self.total_quantity =   total_quantity
         self.limit_price    =   limit_price
 
