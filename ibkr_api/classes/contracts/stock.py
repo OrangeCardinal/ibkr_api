82bdfbbf5dc88920cf998a815c2119840333a6af
OrangeCardinal <david.scott.orkin@gmail.com>
2019-02-09 10:57:32 -0700

    Added the Stock specific __str__ function.

--- ibkr_api/classes/contracts/stock.py
+++ ibkr_api/classes/contracts/stock.py
@@ -13,4 +13,32 @@ class Stock(Contract):
         del self.strike
         del self.right
         del self.multiplier   # Technically is 1, not sure if that is "logically" more correct than removing it
+
+
+    def __str__(self, title="Stock"):
+        """
+        Produces a human readable representation of a Contract
+        :return:
+        """
+        desc =  "\n{0}\n".format(title)
+        desc +=  "--------\n"
+        desc += "Contract ID: {0}\n".format(self.id)
+        desc += "Symbol: {0}\n".format(self.symbol)
+        desc += "Security Type: {0}\n".format(self.security_type)
+        desc += "Exchange: {0}\n".format(self.exchange)
+        desc += "Local Symbol: {0}\n".format(self.local_symbol)
+
+
+        if len(self.regular_trading_hours) > 0:
+            desc += "\nRegular Trading Hours\n"
+            desc += "---------------------\n"
+            for day in self.regular_trading_hours.keys():
+                info = self.regular_trading_hours[day]
+                if info['market_open']:
+                    desc += "{0}:{1} - {2}:{3}\n".format(info['start_date'],info['start_time'],
+                                                       info['end_date'],info['end_time'])
+                else:
+                    desc += "{0}: Closed\n".format(day)
+
+        return desc
         
